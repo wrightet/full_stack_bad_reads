1d33565c814b80236ef70bf2a7868707cc3f95b8
wrightet <wrightet707@gmail.com>
2020-01-07 10:11:32 -0800

    Set up basic user and session controller

---
+++ app/models/user.rb
@@ -0,0 +1,37 @@
+class User < ApplicationRecord
+    validates :username, :session_token, :password_digest, :email, :first_name,
+     :last_name, presence: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+    after_initialize :ensure_session_token
+    attr_reader :password
+
+    has_many :books
+    has_many :reviews
+    has_many :book_shelves
+
+    def self.find_by_credentials
+        user = User.find_by(email: email)
+        return nil unless user && user.is_password?(password)
+        user
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save
+        self.session_token
+    end
+
+    private
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base6
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:new, :create]
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
