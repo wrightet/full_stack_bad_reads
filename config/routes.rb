931dbfdf977acb4d70aad59b1948af19eaccabb7
wrightet <wrightet707@gmail.com>
2020-01-07 10:58:36 -0800

    Create static pages controller. Correct route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-
+  
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:new, :create]
-    resource :session, only [:create, :new, :destroy]
+    resource :session, only: [:create, :new, :destroy]
   end
-  
+  root 'static_pages#root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- frontend/badReads.jsx
+++ frontend/badReads.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { HashRouter, Route} from 'react-router-dom'
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     // const store = configureStore();
-    ReactDOM.render(<Root/>, root)
+    ReactDOM.render(<h1>React is working</h1>, root)
 })
