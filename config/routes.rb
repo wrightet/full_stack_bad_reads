6e1882f62c3bf629b60ec8399891a759a2b66062
wrightet <wrightet707@gmail.com>
2020-01-07 10:28:06 -0800

    Set up Session routes and frontend entry file

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:new, :create]
+    resource :session, only [:create, :new, :destroy]
   end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
