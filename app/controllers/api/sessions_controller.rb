eede1c532defdaa8332a029d37996f0d1c55777a
Ethan Wright <wrightet707@gmail.com>
2020-01-08 21:00:43 -0800

    fix login issues, css

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,8 +2,7 @@ class Api::SessionsController < ApplicationController
 
     def create
         @user = User.find_by_credentials(
-        params[:user][:first_name],
-        params[:user][:last_name],
+     
         params[:user][:email],
         params[:user][:password]
         )
