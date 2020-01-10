d359afaea12578033dc0ce8b04e01d1520fdba89
wrightet <wrightet707@gmail.com>
2020-01-09 17:06:09 -0800

    nav-bar css

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// import { FontAweSomeIcon } from '@fortawesome/react-fontawesome';
+// import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
-// const handleDropDown = () => {
-//     return (<label> Drop Down
-//         <select name="profile" id="profile-drop-down">
-//             <option disabled selected></option>
-//             <option>Welcome, {currentUser.firstName}</option>
-//             <option value="" onClick={logout}>Log out</option>
-//         </select>
-//     </label>
-//     )
-// }
+
 
 const Greeting = ({ currentUser, logout}) => {
+    // Element = <FontAweSomeIcon icon={faUserCircle}/>
+    let hide = "shown"
+    const handleDropDown = () => {
+        console.log('hello')
+        if( hide === 'hidden'){
+            hide = 'shown';
+
+        } else { hide = 'hidden'}
+        }
+    
     const sessionLinks = () => (
         <nav className="login-signup">
             <Link to="/login">Login</Link>
@@ -21,15 +24,19 @@ const Greeting = ({ currentUser, logout}) => {
         </nav>
     )
         window.currentUser = currentUser
-    const personalGreeting = () => (
+      const personalGreeting = () => (
         <div className="greeting-header">
             <ul className="header-left">
-                <li id="header-el">Home</li>
+                <li id="header-el"><Link to="/">Home</Link></li>
                 <li id="header-el">My Books</li>
                 <li id="header-el">Browse</li>
-                <li id="header-el-name"><ul><li>Welcome, {currentUser.firstName}</li>
-                <li><button className="logout-button-header" onClick={logout}>Log out</button></li>
-                </ul></li>
+                <li id="header-el-name">
+                    <ul id="user-dropdown" >
+                        <li onClick={handleDropDown}>User</li>
+                        <li id={hide}>Welcome, {currentUser.firstName}</li>
+                        <li id={hide}><button className="logout-button-header" onClick={logout}>Log out</button></li>
+                    </ul>
+                </li>
             </ul>
             
             
@@ -37,6 +44,61 @@ const Greeting = ({ currentUser, logout}) => {
     )
 
     return currentUser ? personalGreeting() : sessionLinks();
+    
 }
+
+// class Greeting extends React.Component {
+//     constructor(props){
+//         this.state = this.props;
+//         this.hide = "shown"
+//         this.handleDropDown = this.handleDropDown.bind(this)
+//     }
+//     // componentDidMount(){
+
+//     // }
+//     // componentWillUnmount(){
+
+//     // }
+//     handleDropDown = () => {
+//         console.log('hello')
+//         if (hide === 'hidden') {
+//             hide = 'shown';
+
+//         } else { hide = 'hidden' }
+//     }
+
+//     sessionLinks = () => (
+//         <nav className="login-signup">
+//             <Link to="/login">Login</Link>
+//             &nbsp;or&nbsp;
+//             <Link to="/signup">Sign Up</Link>
+//         </nav>
+//     )
+//     // window.currentUser = currentUser
+//     personalGreeting = () => (
+//         <div className="greeting-header">
+//             <ul className="header-left">
+//                 <li id="header-el"><Link to="/">Home</Link></li>
+//                 <li id="header-el">My Books</li>
+//                 <li id="header-el">Browse</li>
+//                 <li id="header-el-name">
+//                     <ul id="user-dropdown" >
+//                         <li onClick={handleDropDown}>User</li>
+//                         <li id={hide}>Welcome, {currentUser.firstName}</li>
+//                         <li id={hide}><button className="logout-button-header" onClick={logout}>Log out</button></li>
+//                     </ul>
+//                 </li>
+//             </ul>
+
+
+//         </div>
+//     )
+
+//     render() {
+//         return currentUser ? personalGreeting() : sessionLinks();
+//     }
+
+   
+// }
 
 export default Greeting;
