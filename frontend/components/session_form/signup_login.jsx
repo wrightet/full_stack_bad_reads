acd522800078182da824208dde6d846394ebd73c
Ethan Wright <wrightet707@gmail.com>
2020-01-12 13:10:44 -0800

    auth css update

---
+++ frontend/components/session_form/signup_login.jsx
@@ -0,0 +1,11 @@
+import LoginFormContainer from './login_form_container';
+import SignupFormContainer from './signup_form_container';
+import React from 'react';
+const homePageSignIn = () => (
+    <div>
+        <LoginFormContainer/>
+        <SignupFormContainer/>
+    </div>
+)
+
+export default homePageSignIn;
