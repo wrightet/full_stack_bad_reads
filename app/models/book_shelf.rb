fab16c4a29e2ef5004aa2c1c6bfcc31560607cd0
wrightet <wrightet707@gmail.com>
2020-01-12 14:56:19 -0800

    Add validations for review and book_shelf

--- app/models/book_shelf.rb
+++ app/models/book_shelf.rb
@@ -1,4 +1,7 @@
 class BookShelf < ApplicationRecord
+    validates :name, presence: true
+   
+
     belongs_to :user
     belongs_to :library
     has_many :books,
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
     validates :rating, inclusion: {in: [1..5], allow_nil: true}
     validates :body, allow_blank: true
+    
     belongs_to :book
     belongs_to :user
 end
