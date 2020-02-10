f0b3e021e9832ccda650b7ef00cccc517a66463a
Ethan Wright <wrightet707@gmail.com>
2020-02-10 11:05:54 -0800

    splash reformat

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -8,50 +8,33 @@ class Splash extends React.Component {
     render() {
         return (
             <div className='splash'>
-
-                <div className='left-content'>
-                    <p>Has a friend or relative ever recommended a book to you, and you hated it?</p>
-                    <p>Did it make you feel like they did not really know you?</p>
-
-                    <p>Did you burn the book, give up on reading, and go live in the woods?</p>
-
-                    <p>Never again!</p>
-
-                    <p>Here at Bad Reads we allow users, just like you, to share their worst experiences with books.</p>
-                    <p>By writing a review you could save someone else the agony you went through.</p>
-
-                    <p>So save a book, and a life, write a review today!</p>
-                </div>
-                <div className='right-content'>
-                    <p>
-                        Deciding what not to read next? We can help you!
-
-                   </p>
-                    <p>
-                        Come browse our inventory of bad books.
-                   </p>
-                    <p>
-                        Great Gatsby? Got it!
-                        <img src={window.images.gatsbyURL} className="splash-book" />
-                    </p>
-                    <p>
-                        Moby Dick? Got it!
-                         <img src={window.images.mobyURL} className="splash-book" />
-                    </p>
-                    <p>
-                        Anything Shakespeare? Of course!
-                        <ul className="shakespeare-splash">
-                            <li><img src={window.images.romeoURL} className='shakespeare-book'/></li>
-                            <li><img src={window.images.midsummerURL} className='shakespeare-book' /></li>
-                            <li><img src={window.images.tempestURL} className='shakespeare-book' /></li>
+                <h3 className="splash-greet">Welcome to Bad Reads!</h3>
+                <ul>
+                    <li>
+                        <ul className="books-list-splash">
+                            <li><img src={window.images.romeoURL} className="splash-book"  /></li>
+                            <li><img src={window.images.midsummerURL} className="splash-book" /></li>
+                            <li><img src={window.images.tempestURL} className="splash-book" /></li>
                         </ul>
-                    </p>
-                    <p>
-                        We have all of those
-                   bad books and dozens more. Come and check them out today!
-                    </p>
+                    </li>
+                    <li>
+                        <ul className="books-list-splash">
+                            <li><img src={window.images.gatsbyURL} className='splash-book'/></li>
+                            <li><img src={window.images.mobyURL} className='splash-book'/></li>
+                            <li><img src={window.images.catcherURL} className='splash-book'/></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <ul className="books-list-splash">
+                            <li><img src={window.images.twilightURL} className='splash-book'/></li>
+                            <li><img src={window.images.fiftyURL} className='splash-book'/></li>
+                            <li><img src={window.images.eragonURL} className='splash-book'/></li>
+                        </ul>
+                    </li>
+                </ul>
+                   
 
-                </div>
+                
 
             </div>
         )
