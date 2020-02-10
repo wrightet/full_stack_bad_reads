ff48f7cc138073525c16735e5b58fecdd5f535e9
Ethan Wright <wrightet707@gmail.com>
2020-02-10 15:47:46 -0800

    make book index show correct cover

--- frontend/components/books/book_index_item.jsx
+++ frontend/components/books/book_index_item.jsx
@@ -5,13 +5,34 @@ import BookIndexContainer from './book_index_container';
 class BookIndexItem extends React.Component {
     constructor(props){
         super(props)
+        this.keyFinder = this.keyFinder.bind(this)
     }
 
+    keyFinder(url){
+        let val = url.split('.')[0];
+        let keys = Object.keys(window.images)
+        let newKeys = keys.map(key => {
+            return key.split('URL')[0];
+        })
+        let values = Object.values(window.images)
+    
+       for (let i = 0; i < keys.length; i++) {
+           if (val === newKeys[i]){
+               return window.images[keys[i]]
+           } 
+               
+           
+           
+       }
+        return window.images.openbookURL;
+       
+    }
     render(){
-        
+       
+        let book = this.props.book
         return (
             <tr className="book-row">
-                <td><Link to={`/books/${book.id}`}><img src={window.images.openbookURL} alt="" className="book-photo" /></Link></td>
+                <td><Link to={`/books/${book.id}`}><img src={this.keyFinder(book.url)} alt="" className="book-photo" /></Link></td>
                 <td>{book.title}</td>
                 <td>{book.author}</td>
                 <td>{book.genre}</td>
