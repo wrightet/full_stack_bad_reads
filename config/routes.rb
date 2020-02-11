7129e2c76b43cb5c1ee45c4e9b2ef17d060dd5c5
Ethan Wright <wrightet707@gmail.com>
2020-02-11 11:48:22 -0800

    create util file with ajax calls for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [ :create]
     resource :session, only: [:create, :new, :destroy]
-    resources :books, only: [:create, :show, :index, :update]
-    resources :reviews, only: [:create, :index, :update]
+    resources :books, only: [:create, :show, :index, :update] do
+      resources :reviews, only: [:create, :index, :update]
+    end
+    
   end
   root 'static_pages#root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
---
+++ frontend/util/review_util.js
@@ -0,0 +1,24 @@
+export const fetchAllReviews = (bookId) => (
+    $.ajax({
+        method: 'GET',
+        url: `/api/books/${bookId}/reviews`
+    })
+)
+
+export const createReview = (bookId, review) => (
+    $.ajax({
+        method: 'POST',
+        url: ` /api/books/${bookId}/reviews`,
+        data: {review}
+    })
+)
+
+export const updateReview = (bookId, review) => (
+    $.ajax({
+        method:'PATCH',
+        url: `/api/books/${bookId}/reviews/${review.id}`,
+        data: {review},
+        contentType: false,
+        processType: false
+    })
+)
