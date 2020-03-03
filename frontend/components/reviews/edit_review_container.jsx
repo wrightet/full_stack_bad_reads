9fbbaa2e3248bb1738c5123c1fa02edf30dea5fc
Ethan Wright <wrightet707@gmail.com>
2020-03-02 17:36:51 -0800

    make edit form for reviews

---
+++ frontend/components/reviews/edit_review_container.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { requestReview, updateReview } from '../../actions/review_actions';
+import ReviewForm from './review_form';
+
+class EditReviewForm extends React.Component {
+    componentDidMount(){
+        this.props.requestReview(this.props.match.params.bookId)
+    }
+
+    render(){
+
+        const {action, formType, review} = this.props;
+
+        if (!review) return null;
+
+        return (
+            <ReviewForm
+                action={action}
+                formType={formType}
+                review={review}
+            />
+        )
+    }
+}
+
+const mSTP = (state, ownProps) => ({
+    review: state.entities.reviews[ownProps.match.params.reviewId],
+    formType: 'Edit Review'
+})
+
+const mDTP = dispatch => ({
+    requestReview: bookId => dispatch(requestReview(bookId)),
+    action: (bookId, review) => dispatch(updateReview(bookId, review))
+})
+
+export default connect(mSTP, mDTP)(EditReviewForm);
