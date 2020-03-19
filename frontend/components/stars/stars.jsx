3782df6db96c6d097f93b905010bfed31ea7781c
Ethan Wright <wrightet707@gmail.com>
2020-03-19 14:39:16 -0700

    try to make them stars instead of radio bubbles

--- frontend/components/stars/stars.jsx
+++ frontend/components/stars/stars.jsx
@@ -1,74 +1,4 @@
-// import React from 'react'
-
-// const Range = (min, max) => Array(max - min + 1).fill().map((_,i) => min += i);
-
-// const ItemRated = ({ checked, colored, onChange, value}) => (
-//     <label 
-//         className={`'rated-item ${colored ? 'rating-item-selected' : ''}`}
-//     >
-//         <input
-//             checked={checked}
-//             className='rate-input'
-//             onChange={(e) => { onChange(value)}}
-//             type="radio"
-//             value={value}
-
-        
-//         />
-//     </label>
-// )
-
-// export const Rating = ({min, max, onChange, value}) => {
-
-//     return (
-
-//         <div className='rating'>
-//             {Range(min,max).map( item => (
-//                <ItemRated
-//                     key={item}
-//                     colored={value >= item}
-//                     checked={value === item}
-//                     value={item}
-//                     onChange={onChange}
-//                 />
-
-//             ))}
-//         </div>
-//     )
-// }
-
-// const IndivRange = (min, max) => Array(max - min + 1).fill().map((_, i) => min + i)
-
-// const IndivRatingItem = ({ checked, colored, value }) => (
-//     <label
-//         className={`rating-indiv-item ${colored ? 'rating-item-selected-t' : ''}`} >
-//         <input
-//             checked={checked}
-//             className='rating-input'
-//             type="radio"
-//             value={value}
-//             readOnly
-//         />
-//     </label>
-// )
-
-// export const IndivRating = ({ min, max, value }) => {
-//     return (
-//         <div className='rating-y' >
-//             {IndivRange(min, max).map(item => (
-//                 <IndivRatingItem key={item}
-//                     colored={value >= item}
-//                     checked={value === item}
-//                     value={item}
-//                 />
-//             ))
-//             }
-//         </div>
-//     )
-// }
 import React from 'react';
-
-
 
 const Range = (min, max) => Array(max - min + 1).fill().map((_, i) => min + i)
 
@@ -122,7 +52,7 @@ const IndivRatingItem = ({ checked, colored, value }) => (
 
 export const IndivRating = ({ min, max, value }) => {
     return (
-        <div className='rating-y' >
+        <div className='stars' >
             {IndivRange(min, max).map(item => (
                 <IndivRatingItem key={item}
                     colored={value >= item}
