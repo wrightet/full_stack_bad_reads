274562881ff8364e4744a8ecea4cbadd45a370d5
Ethan Wright <wrightet707@gmail.com>
2020-03-23 15:33:07 -0700

    change header logo

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -23,7 +23,7 @@ const App = () => (
         <header className="nav-bar">
             <Link to="/" className="header-link">
                 <div className='logo'>
-                     <h1 classNAme='bad'>Bad</h1><h1 className='reads'>Reads</h1>
+                     <h1 classNAme='bad'>bad</h1><h1 className='reads'>reads</h1>
                 </div>
                
             </Link>
