83b0637ad92d2870799784d5c1cc78f2d10e7452
Ethan Wright <wrightet707@gmail.com>
2020-04-08 21:42:14 -0700

    shelf util

---
+++ frontend/util/shelves_util.js
@@ -0,0 +1,36 @@
+export const fetchAllShelves = () => (
+    $.ajax({
+        method: 'GET',
+        url: '/api/book_shelves'
+    })
+)
+
+export const fetchShelf = id => (
+    $.ajax({
+        method: 'GET',
+        url: `/api/book_shelves/${id}`
+    })
+)
+
+export const createShelf = (shelf) => (
+    $.ajax({
+        method:'POST',
+        url: '/api/book_shelves',
+        data: {shelf}
+    })
+)
+
+export const updateShelf = (shelf) => (
+    $.ajax({
+        method: 'PATCH',
+        url: `/api/book_shelves/${shelf.id}`,
+        data: {shelf}
+    })
+)
+
+export const deleteShelf = shelfId => (
+    $.ajax({
+        method:'DELETE',
+        url:`/api/book_shelves/${shelfId}`
+    })
+)
