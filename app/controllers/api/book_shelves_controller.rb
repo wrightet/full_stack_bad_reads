6dfa7106c6aa98511bcca7ec64680a2a7b211b35
Ethan Wright <wrightet707@gmail.com>
2020-04-28 13:59:24 -0700

    change the methods in library so that they fit better

--- app/controllers/api/book_shelves_controller.rb
+++ app/controllers/api/book_shelves_controller.rb
@@ -36,26 +36,26 @@ class Api::BookShelvesController < ApplicationController
         render :show
     end
 
-    def add_book
-        book_shelf = BookShelf.new(
-            book_id: params[:book_id],
-            shelf_id: params[:shelf_id]
-        )
-        if book_shelf.save
-            render json: book_shelf, status: :created
-        else
-            render json: book_shelf.errors.full_messages, status: :unprocssable_entity
-        end
-    end
+    # def add_book
+    #     book_shelf = BookShelf.new(
+    #         book_id: params[:book_id],
+    #         shelf_id: params[:shelf_id]
+    #     )
+    #     if book_shelf.save
+    #         render json: book_shelf, status: :created
+    #     else
+    #         render json: book_shelf.errors.full_messages, status: :unprocssable_entity
+    #     end
+    # end
 
-    def remove_book
-        shelf = BookShelf.find_by(
-            book_id: params[:book_id],
-            shelf_id: params[:shelf_id]
-        )
-        shelf.destroy
-        render json: shelf
-    end
+    # def remove_book
+    #     shelf = BookShelf.find_by(
+    #         book_id: params[:book_id],
+    #         shelf_id: params[:shelf_id]
+    #     )
+    #     shelf.destroy
+    #     render json: shelf
+    # end
 
     private
     def book_shelf_params
