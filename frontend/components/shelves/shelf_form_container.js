24c6f888c160367ee325ba185fd2351089817df7
Ethan Wright <wrightet707@gmail.com>
2020-05-12 15:19:52 -0700

    changing shelf form container

--- frontend/components/shelves/shelf_form_container.js
+++ frontend/components/shelves/shelf_form_container.js
@@ -2,6 +2,7 @@ import { connect } from'react-redux';
 import ShelfForm from './shleves_form';
 import {requestBook} from '../../actions/book_actions';
 import {requestAllShelves, createShelf } from '../../actions/shelf_actions';
+import {placeOnShelf, takeFromShelf} from '../../actions/shelved_book_actions';
 
 const mSTP = (state, ownProps) => {
     const book = state.entities.books[ownProps.match.params.id];
@@ -18,7 +19,9 @@ const mSTP = (state, ownProps) => {
 const mDTP = dispatch => ({
     requestBook: id => dispatch(requestBook(id)),
     requestAllShelves: () => dispatch(requestAllShelves()),
-    createShelf: shelf => dispatch(createShelf(shelf))
+    createShelf: shelf => dispatch(createShelf(shelf)),
+    placeOnShelf: book => dispatch(placeOnShelf(book)),
+    takeFromShelf: book => dispatch(takeFromShelf(book)
 })
 
 export default connect(mSTP,mDTP)(ShelfForm);
