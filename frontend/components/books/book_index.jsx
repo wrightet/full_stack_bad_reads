f43a20dd3ffa3ba27a63a68b2286856e105e69e1
Ethan Wright <wrightet707@gmail.com>
2020-06-10 17:21:36 -0700

    remove useless code

--- frontend/components/books/book_index.jsx
+++ frontend/components/books/book_index.jsx
@@ -122,39 +122,3 @@ class BookIndex extends React.Component {
 }
 
 export default BookIndex;
-
-
-    // render(){
-    //     const {books} = this.props;
-    //     if (!books){return null;}
-    //     return(
-    //     <div className="book-table-div">
-
-    //         <table className="books-table">
-    //             <thead className="book-index-column-list">
-    //                 <tr className="table-header-row">
-    //                     <th className="book-table-header-column-cover">Cover</th>
-    //                     <th className="book-table-header-column-name">Title</th>
-    //                     <th className="book-table-header-column-name">Author</th>
-    //                     <th className="book-table-header-column-rating">Rating</th>
-    //                     <th className="book-table-header-column-review">Review</th>
-    //                     <th className="book-table-header-column-genre">Genre</th>
-
-
-    //                 </tr>
-    //             </thead> 
-    //              <tbody className='books-list-body'>
-    //                  { 
-    //                         books.map(book => (
-    //                             <BookIndexItem
-    //                             book = {book}
-    //                             key = {book.id}
-    //                             requestAllReviews = {this.props.requestAllReviews}
-    //                            />
-    //                         ))
-    //                     }
-    //              </tbody>
-    //         </table>
-    //     </div>
-    //     )
-    // }
