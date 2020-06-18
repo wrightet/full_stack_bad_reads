1423a4ed5ca2b0b3b8e3b8f7f934308d16586132
Ethan Wright <wrightet707@gmail.com>
2020-06-17 21:24:37 -0700

    photo validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,13 @@ class Book < ApplicationRecord
     validates :title, :author, :genre, presence: true
     validates :read, :currently_reading, :want_to_read, 
     inclusion: {in: [true, false], message: "value must be true or false"}
+#     validates :ensure_photo
+
+#     def ensure_photo
+#           unless self.photo.attached?
+#                errors[:photo] << 'must attach photo'
+#           end
+#     end
 
      has_many :reviews,
           foreign_key: :book_id,
